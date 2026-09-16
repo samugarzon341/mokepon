e5ce5e60aaf1e31216fc7b544bc470097ec8832d
Samuel <samuelgarzonrojas@gmail.com>
2026-09-16 18:52:02 -0500

    a

--- js/mokepon.js
+++ js/mokepon.js
@@ -448,7 +448,18 @@ function enviarPosicion(x, y) {
            y,
         })
     })
+    .then(function (res) {
+        if (res.ok) {
+            res.json()
+            .then(function ({enemigos}){
+                console.log(enemigos)
+            })
+        }
+    })
 }
+        
+               
+
 
 
 function moverDerecha() {
